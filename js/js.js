2b07b33beae720261e3b4b42b90f2c9e9014907b
kharkovstiger <kharkovs.tiger@gmail.com>
2017-10-09 09:26:52 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,31 @@
+var app = angular.module('obbtm', []);
+
+app.controller('obbtmCtrl', function ($scope, $http) {
+
+    var baseURL='http://bbapi.buzzerbeater.com';
+    var NTIds=[39613679, 40457235];
+
+    $scope.tl=[];
+
+    $http.get(baseURL+'/login.aspx?login=lnrstgr&code=katana').then(
+        function (response) {
+            console.log(response.status+", "+response.data);
+        },
+        function (response) {
+            console.log(response.status+", "+response.data);
+        }
+    );
+
+    $scope.getTLforNT=function () {
+        for(var i=0;i<NTIds.length;i++){
+            $http.get(baseURL+'/player.aspx?playerid='+NTIds[i]).then(
+                function (response) {
+                    console.log(response.status+", "+response.data);
+                },
+                function (response) {
+                    console.log(response.status+", "+response.data);
+                }
+            );
+        }
+    }
+});
