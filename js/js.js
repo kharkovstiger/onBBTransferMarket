5329126e80745f1b303059875b5cb65980e7bfd1
kharkovstiger <kharkovs.tiger@gmail.com>
2017-10-09 12:18:08 +0300

    http

--- js/js.js
+++ js/js.js
@@ -2,30 +2,37 @@ var app = angular.module('obbtm', []);
 
 app.controller('obbtmCtrl', function ($scope, $http) {
 
+    // var baseURL='http://localhost:8080/api';
     var baseURL='http://bbapi.buzzerbeater.com';
+
     var NTIds=[39613679, 40457235];
 
     $scope.tl=[];
 
-    $http.get(baseURL+'/login.aspx?login=lnrstgr&code=katana').then(
-        function (response) {
-            console.log(response.status+", "+response.data);
-        },
-        function (response) {
-            console.log(response.status+", "+response.data);
-        }
-    );
+    $.get( baseURL+'/login.aspx?login=lnrstgr&code=katana', function( data ) {
+        console.log(data);
+    });
 
-    $scope.getTLforNT=function () {
-        for(var i=0;i<NTIds.length;i++){
-            $http.get(baseURL+'/player.aspx?playerid='+NTIds[i]).then(
-                function (response) {
-                    console.log(response.status+", "+response.data);
-                },
-                function (response) {
-                    console.log(response.status+", "+response.data);
-                }
-            );
-        }
-    }
+    // // $http.get(baseURL+'/login?login=lnrstgr&code=katana').then(
+    // $http.get(baseURL+'/login.aspx?login=lnrstgr&code=katana').then(
+    //     function (response) {
+    //         console.log(response.status+", "+response.data);
+    //     },
+    //     function (response) {
+    //         console.log(response.status+", "+response.data);
+    //     }
+    // );
+    //
+    // $scope.getTLforNT=function () {
+    //     for (var i=0;i<NTIds.length;i++){
+    //         $http.get(baseURL+'/player.aspx?playerid='+NTIds[i]).then(
+    //             function (response) {
+    //                 console.log(response.status+", "+response.data);
+    //             },
+    //             function (response) {
+    //                 console.log(response.status+", "+response.data);
+    //             }
+    //         );
+    //     }
+    // }
 });
